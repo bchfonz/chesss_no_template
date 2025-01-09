769ee550edf2821639c46bc0b5a3ea50184edf0f
Benjamin Fonbuena <bchfonz@gmail.com>
2025-01-08 19:13:16 -0700

    Initialized getters and setters and started on piece logic

--- chess/shared/src/main/java/chess/ChessBoard.java
+++ chess/shared/src/main/java/chess/ChessBoard.java
@@ -7,7 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
+    private ChessPiece[][] board = new ChessPiece[8][8];
     public ChessBoard() {
         
     }
@@ -19,7 +19,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +30,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()][position.getColumn()];
     }
 
     /**
@@ -38,6 +38,89 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                //Sets major white pieces
+                if(i == 0){
+                    //White Rooks
+                    if(j == 0 || j == 7){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+                    //White Knights
+                    if(j == 1 || j == 6){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+                    //White Bishops
+                    if(j == 2 || j== 5){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+                    //White Queen
+                    if(j == 3){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+                    //White King
+                    if(j == 4){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+
+                }
+                //Sets major Black pieces
+                if(i == 7){
+                    //Black Rooks
+                    if(j == 0 || j == 7){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+                    //Black Knights
+                    if(j == 1 || j == 6){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+                    //Black Bishops
+                    if(j == 2 || j== 5){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+                    //Black Queen
+                    if(j == 3){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+                    //Black King
+                    if(j == 4){
+                        ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+                        ChessPosition tempPosition = new ChessPosition(i, j);
+                        addPiece(tempPosition, tempPiece);
+                    }
+
+                }
+                //Sets white pawns
+                if(i == 1){
+                    ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+                    ChessPosition tempPosition = new ChessPosition(i, j);
+                    addPiece(tempPosition, tempPiece);
+                }
+                //Sets black pawns
+                if(i == 6){
+                    ChessPiece tempPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+                    ChessPosition tempPosition = new ChessPosition(i, j);
+                    addPiece(tempPosition, tempPiece);
+                }
+            }
+        }
     }
 }
--- chess/shared/src/main/java/chess/ChessPiece.java
+++ chess/shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -10,7 +11,11 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType type;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +34,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +52,53 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> legalMoves = new ArrayList<>();
+        ChessPosition tempPosition = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+        ChessPosition tempPosition1 = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+        ChessPosition tempPosition2 = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+        ChessPosition tempPosition3 = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+        if(board.getPiece(myPosition).type == ChessPiece.PieceType.ROOK){
+
+        }
+        else if(board.getPiece(myPosition).type == ChessPiece.PieceType.KNIGHT){
+
+        }
+        //Generates legal moves for Bishop
+        else if(board.getPiece(myPosition).type == ChessPiece.PieceType.BISHOP){
+            //TODO:Figure out how many times to loop
+            for(int i = myPosition.getRow(); i < (myPosition.getRow()) - 8; i++){
+                int tempRow = myPosition.getRow();
+                int tempCol = myPosition.getColumn();
+                //Will this mess up memory allocation?
+                if(board.getPiece(tempPosition) != null){
+
+                }
+                if(board.getPiece(tempPosition1)!= null){
+
+                }
+                if(board.getPiece(tempPosition2)!= null){
+
+                }
+                if(board.getPiece(tempPosition3)!= null){
+
+                }
+            }
+        }
+        else if(board.getPiece(myPosition).type == ChessPiece.PieceType.QUEEN){
+
+        }
+        else if(board.getPiece(myPosition).type == ChessPiece.PieceType.KING){
+
+        }
+        else if(board.getPiece(myPosition).type == ChessPiece.PieceType.PAWN){
+
+        }
+
+
+        return legalMoves;
     }
+    /**
+     * Bishop moves
+     * @returns collection of valid Bishop Moves
+     */
 }
